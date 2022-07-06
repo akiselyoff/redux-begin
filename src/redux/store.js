946ea7da72ad038e306528b27fc7682efda176a9
kesel <a.kiselyoff@gmail.com>
2022-07-06 15:41:06 +0300

    prepare Counter for connect to Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
 
-const reducer = (state = {}, action) => {
+const reducer = (state = { a: 5 }, action) => {
   console.log('Log action in reducer: ', action);
   return state;
 };
+
 const store = createStore(reducer);
 
 export default store;
