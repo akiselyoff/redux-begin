384c72f86c29e12a898a15f69faf656244cd23ef
kesel <a.kiselyoff@gmail.com>
2022-07-09 15:00:43 +0300

    clean some extra code

--- src/redux/counter/counter-reducer.js
+++ src/redux/counter/counter-reducer.js
@@ -1,19 +1,22 @@
 import { combineReducers } from 'redux';
 import actionTypes from './counter-types';
 
-const valueReducer = (state = 10, { type, payload }) => {
+const initialValue = 0;
+
+const valueReducer = (state = initialValue, { type, payload }) => {
   switch (type) {
     case actionTypes.INCREMENT:
       return state + payload;
 
     case actionTypes.DECREMENT:
       return state - payload;
+
     default:
       return state;
   }
 };
 
-const stepReducer = (state = 5, action) => state;
+const stepReducer = (state = 2, action) => state;
 
 export default combineReducers({
   value: valueReducer,
